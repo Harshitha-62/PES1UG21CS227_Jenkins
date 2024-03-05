82f8ff8c0aab4832e85546b8e5e5ea53dd6ef7f7
Harshitha-62 <127194148+Harshitha-62@users.noreply.github.com>
2024-03-05 22:24:20 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,36 +1,10 @@
-// //File: hello.cpp
-// #include <iostream>
-// using namespace std;
+File: hello.cpp
+#include <iostream>
+using namespace std;
 
-// int main(void) {
-//   cout << "Hello, World\n";
-//   cout << "Hello, Jenkins\n";
-//   cout << "I have successfully built and run\n";
-//   return 0;
-// }
-pipeline {
-  agent any
-  stages {
-    stage('Build') {
-      steps {
-        build 'PES1UG21CS227-1'
-        sh 'g++ main.cpp -o output'
-      }
-    }
-    stage('Test') {
-      steps {
-        sh './output'
-      }
-    }
-    stage('Deploy') {
-      steps {
-        echo 'deploy'
-      }
-    }
-  }
-  post{
-    failure{
-      error 'Pipeline failed'
-    }
-  }
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
 }
